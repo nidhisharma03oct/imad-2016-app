029d5d10dae404402e842101b79d53e7973dcdd2
nidhisharma03oct <nidhisharma03oct@gmail.com>
2016-09-26 00:41:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,12 +67,12 @@ app.get('/article-one',function(req,res){
 
 app.get('/article-two',function(req,res){
     //res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-     res.send('Article one requested and will be served here.');
+     res.send('Article two requested and will be served here.');
 });
 
 app.get('/article-three',function(req,res){
    // res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-    res.send('Article one requested and will be served here.');
+    res.send('Article three requested and will be served here.');
 });
 
 app.get('/ui/style.css', function (req, res) {
