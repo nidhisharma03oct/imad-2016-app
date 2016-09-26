bd2fd1cd2e9a9dbb430727fb117c0f45809fabda
nidhisharma03oct <nidhisharma03oct@gmail.com>
2016-09-26 17:19:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,8 +85,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req,res){
     //articleName== article-one
     //articles[articleName]=={} content object for article one
- 
- 
+
     articleName= req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 
