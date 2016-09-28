47763132c2cec7fae351e11952605a9fab8a2413
nidhisharma03oct <nidhisharma03oct@gmail.com>
2016-09-28 13:33:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,15 @@ var element= document.getElementById('main-text');
 element.innerHTML= 'New Value';
 
 var img= document.getElementById('girl');
-img.onclick= function(){
  
-  img.style.marginLeft='350px';
+ var marginLeft=0;
+ function moveRight(){
+     var marginLeft= marginLeft+ 10;
+     img.style.marginLeft= marginLeft +'px'
+ }
+
+img.onclick= function(){
+   var interval= setInterval(moveRight,100);
+ // img.style.marginLeft='350px';
 
 };
