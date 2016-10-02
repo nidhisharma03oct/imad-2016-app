4a910e484ac3afda98e7c67bbe469fc877ddd68e
nidhisharma03oct <nidhisharma03oct@gmail.com>
2016-10-02 20:50:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,7 +53,7 @@ var submit= document.getElementById('submit-btn');
   //capture a list of names and render it as a list
    var names=['name1','name2','name3','name4'];
    var list='';
-   for(var i =0;names.lenth;i++)
+   for(var i =0;i<names.length;i++)
    {
        list += '<li>' + name[i]+ '</li>';
    }
