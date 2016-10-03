0dba2d84229c05ed3a42346ef7813d429a43b1ce
nidhisharma03oct <nidhisharma03oct@gmail.com>
2016-10-03 13:25:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,40 +1,5 @@
 console.log('Loaded!');
 
-var submit= document.getElementById('submit-comment');
- submit.onclick= function(){
-  
-   //create a request object
-   
-   var request = new XMLHttpRequest();
-   
-  //capture the response and store it in the variable
-  
-    request.onreadystatechange = function(){
-	if(request.readyState=== XMLHttpRequest.DONE){
-		// take some action
-		 if (request.status===200)
-		 {
-	   var names=request.responseText;
-	   names= JSON.parse(names);
-        var list='';
-        for(var i =0;i<names.length;i++)
-        {
-            list += '<li>' + names[i]+ '</li>';
-         }
-     var ul = document.getElementById('namelist');
-     ul.innerHTML= list;
-		 }
-	}
-	//Not done yet
-	
-};
-//rendering the varaiable in correct span
-     var nameInput=document.getElementById('comments');
-    var name= nameInput.value;
- // make a request to the server and send the name
-  request.open('GET','http://nidhisharma03oct.imad.hasura-app.io/submit-name?name='+ name,true);
-  request.send(null);
- };
 
 // move the image
 /*
@@ -111,16 +76,15 @@ var submit= document.getElementById('submit-btn');
 	//Not done yet
 	
 };
-//rendering the varaiable in correct span
      var nameInput=document.getElementById('name');
-var name= nameInput.value;
+     var name= nameInput.value;
  // make a request to the server and send the name
   request.open('GET','http://nidhisharma03oct.imad.hasura-app.io/submit-name?name='+ name,true);
   request.send(null);
  };
 
 
-// for comment submitting
+
 
 
 
